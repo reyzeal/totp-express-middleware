c1afe55b1be800d0b0b909af24f021b0c20feee2
Rizal Ardhi Rahmadani <rizal.ardhi.rahmadani@gmail.com>
2021-02-15 17:58:43 +0700

    Update

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ const opt = (options = {}) => {
         key : "secret",
         length : 6,
         period : 30,
-        T0 : 0
+        T0 : 0,
+        type : "default"
     },options)
     return options
 }
@@ -14,11 +15,26 @@ module.exports = (option = {})=>{
         const authToken = req.headers['authorization'] && req.headers['authorization'].split(" ")
         if(authToken && authToken.length > 1 && authToken[0] === "Bearer"){
             const token = authToken[1]
-            if(Validate(token,options.key,options.length,options.period,options.T0)){
+            let salt = ""
+            switch (options.type) {
+                case "default":
+                    break;
+                case "body":
+                    salt = (req.body && req.body.length) || ""
+                    break
+                case "path":
+                    salt = req.path
+                    break;
+                default:
+                    throw Error("Salt type is not defined")
+            }
+            if(Validate(token,options.key+salt,options.length,options.period,options.T0)){
                 next()
             }else{
                 throw new Error('Invalid Token');
             }
+        }else{
+            throw new Error('Token not provided');
         }
     }
 }
